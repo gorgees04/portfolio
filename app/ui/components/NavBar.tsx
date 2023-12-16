d6213a2fab22d46943cbcd6a0aaef9495ae84681
Gorgees Odisho <gorgees041@gmail.com>
2023-12-16 06:57:15 +0300

    navbar done

--- app/ui/components/NavBar.tsx
+++ app/ui/components/NavBar.tsx
@@ -18,18 +18,22 @@ const NavBar = () => {
   };
 
   return (
-    <Navbar fluid={true} rounded={true} className="bg-black">
+    <Navbar
+      fluid={true}
+      rounded={true}
+      className="bg-black h-14 pt-2 sm:pt-4 relative"
+    >
       <Navbar.Brand href="https://flowbite.com/">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           Galaxy Coder
         </span>
       </Navbar.Brand>
       <Navbar.Toggle />
-      <Navbar.Collapse>
+      <Navbar.Collapse className="bg-black absolute top-16 left-0 p-2 sm:p-0 sm:static">
         {pages.map((page, id) => {
           console.log("pathname", pathname, "page", page);
           return (
-            <div key={id} className="text-lg font-bold text-center ">
+            <div key={id} className="text-lg font-bold text-center capitalize">
               <Link
                 href={page === "home" ? "/" : `/${page}`}
                 className={selectedPage(page)}
