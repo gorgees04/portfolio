84645457706c08bc76dc441dfb6f22ba6af14b2a
Gorgees Odisho <gorgees041@gmail.com>
2023-12-23 08:22:40 +0300

    change nav bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,93 @@
+import Image from "next/image";
+import dev5 from "@/public/images/dev-5.png";
+import reactLogo from "@/app/ui/img/react-0.png";
+import jsLogo from "@/app/ui/img/js-0.png";
+import tsLogo from "@/app/ui/img/ts-0.png";
+import tailwindLogo from "@/app/ui/img/tailwind-0.png";
+import nodeLogo from "@/app/ui/img/node-1.png";
+import pythonLogo from "@/app/ui/img/python.png";
+import TsParticles from "./ui/components/TsParticles";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Start with portfolio
+    <main className="flex flex-col items-center justify-center p-24">
+      <div>
+        <div className="flex flex-col md:flex-row">
+          <div>
+            <Image
+              src={dev5}
+              alt="home_img"
+              width={600}
+              height={0}
+              className="rounded-md mr-20"
+            />
+          </div>
+          <div className="flex flex-col justify-center">
+            <p className="text-white">This is</p>
+            <h1 className="text-5xl ml-2 my-1 font-extrabold">
+              Gorgees Odisho
+            </h1>
+            <p className="text-2xl text-right ">Web Developer</p>
+          </div>
+        </div>
+      </div>
+      {/* <TsParticles /> */}
+      {/* <div className="flex my-10 justify-around items-center h-[250px]">
+        <div>
+          <Image
+            src={jsLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+        <div>
+          <Image
+            src={tsLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+        <div>
+          <Image
+            src={reactLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+        <div>
+          <Image
+            src={tailwindLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+        <div>
+          <Image
+            src={nodeLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+        <div>
+          <Image
+            src={pythonLogo}
+            alt="home_img"
+            width={80}
+            height={0}
+            className="rounded-md mr-20"
+          />
+        </div>
+      </div> */}
     </main>
   );
 }
