603bed83bd4351584516677a20782b57f7a8a2af
Gorgees Odisho <gorgees041@gmail.com>
2023-12-24 22:33:31 +0300

    project page completed

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,39 +14,43 @@ const Projects = () => {
   return (
     <section className="w-full flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold">Projects</h1>
-      <div>
+      <div className="m-2">
         {projectsData.map((project: Project) => {
           return (
             <div
               key={project.name}
-              className="border-2 border-gBlue m-10 flex flex-col items-center md:flex-row rounded-lg bg-gBlue bg-opacity-10 overflow-hidden p-2"
+              className="border-2 border-gBlue my-14 flex flex-col items-center md:flex-row rounded-lg bg-gBlue bg-opacity-10 overflow-hidden p-2"
             >
               <div className="">
                 <Image
                   src={project.img}
                   alt={project.name}
-                  width={420}
-                  className="border-2 border-gBlue rounded-lg"
+                  width={370}
+                  className="border-2 border-gBlue rounded-lg md:w-[450px] mr-3"
                 />
               </div>
-              <div className="w-[400px] flex flex-col items-center ">
-                <h1 className="text-xl font-bold">{project.name}</h1>
-                <p className="text-gLightBlue text-justify text-sm">
+              <div className="w-[370px] flex flex-col items-center my-3 p-1">
+                <h1 className="text-2xl font-extrabold">{project.name}</h1>
+                <p className="text-gLightBlue text-justify text-sm my-3">
                   {project.descrption}
                 </p>
-                <div className="flex flex-wrap">
+                <div className="flex flex-wrap mb-5">
                   {project.tools.map((tool) => (
-                    <p key={tool} className="mr-2 text-sm">
+                    <p key={tool} className="mr-3 text-bold">
                       {tool}
                     </p>
                   ))}
                 </div>
                 <div className="flex justify-around w-full">
-                  <button className="bg-gray-800 p-2 rounded ">
-                    <a href={project.github}>GitHub</a>
+                  <button className="bg-gray-800 p-2 rounded hover:bg-gray-600">
+                    <a href={project.github} target="_blank">
+                      GitHub
+                    </a>
                   </button>
-                  <button>
-                    <a href={project.demo}>LIVE DEMO</a>
+                  <button className="bg-gray-800 p-2 rounded hover:bg-gray-600">
+                    <a href={project.demo} target="_blank">
+                      LIVE DEMO
+                    </a>
                   </button>
                 </div>
               </div>
