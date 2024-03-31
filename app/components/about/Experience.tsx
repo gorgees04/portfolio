f0451245c262d2265e8a1a46b01e9db0b6785453
Gorgees Odisho <gorgees041@gmail.com>
2024-03-31 19:04:28 +0300

    update resume

--- app/components/about/Experience.tsx
+++ app/components/about/Experience.tsx
@@ -15,7 +15,14 @@ const Experience = () => {
     "CLI/Shell Scripting",
   ];
 
-  const backEnd = ["SQL/NOSQL", "MongoDB", "NodeJS", "ExpressJS", "API"];
+  const backEnd = [
+    "SQL/NOSQL",
+    "MongoDB",
+    "NodeJS",
+    "ExpressJS",
+    "API",
+    "Docker",
+  ];
   return (
     <div className="w-full flex flex-col justify-center items-center px-[10%] md:px-[20%]">
       <h1 className="text-3xl font-bold m-5">Experience</h1>
