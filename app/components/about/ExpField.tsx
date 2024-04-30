66397a6ee4cfe5b086e216bd21c84c762b9d930d
Gorgees Odisho <gorgees041@gmail.com>
2024-05-01 02:09:25 +0300

    update experince section

--- app/components/about/ExpField.tsx
+++ app/components/about/ExpField.tsx
@@ -1,14 +1,39 @@
-const ExpField = ({ title, techs }: { title: string; techs: string[] }) => {
+import Image from "next/image";
+
+const ExpField = ({
+  title,
+  techs,
+}: {
+  title: string;
+  techs: { name: string; img: any }[];
+}) => {
   return (
-    <div className="m-5">
+    <div className="mb-7  bg-gBlue bg-opacity-[0.07] rounded p-1">
       <p className="text-center text-xl font-bold my-2 capitalize">{title}</p>
-      <ul className="p-5 grid grid-cols-3 grid-flow-row gap-2 text-center">
-        {techs.map((exp: string) => (
-          <li key={exp} className={"text-gLightBlue"}>
-            {exp}
-          </li>
+      <div className="py-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 grid-flow-row gap-2 text-center">
+        {techs.map((tech: { name: string; img: string }) => (
+          <div
+            key={tech.name}
+            className="flex flex-col items-center justify-between h-[80px] m-4"
+          >
+            <div className="h-[80%]">
+              <Image
+                src={tech.img}
+                alt={tech.name}
+                width={200}
+                className="w-[60px]"
+              />
+            </div>
+            <div
+              className={
+                "text-gLightBlue flex justify-center items-center h-[20%] capitalize"
+              }
+            >
+              <p>{tech.name}</p>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
--- app/components/about/Experience.tsx
+++ app/components/about/Experience.tsx
@@ -1,36 +1,71 @@
 import ExpField from "./ExpField";
+import jsImg from "@/public/images/icons/javascript.png";
+import tsImg from "@/public/images/icons/typescript.png";
+import pythonImg from "@/public/images/icons/python.png";
+import reactImg from "@/public/images/icons/react.png";
+import nextjsImg from "@/public/images/icons/nextjs.png";
+import reduxImg from "@/public/images/icons/redux.png";
+import htmlImg from "@/public/images/icons/html.png";
+import cssImg from "@/public/images/icons/css.png";
+import tailwindImg from "@/public/images/icons/tailwind.png";
+import bootstrapImg from "@/public/images/icons/bootstrap.png";
+import materialImg from "@/public/images/icons/material-ui.png";
+import nodejsImg from "@/public/images/icons/nodejs.png";
+import expressjsImg from "@/public/images/icons/exoressjs.png";
+import apiImg from "@/public/images/icons/api.png";
+import sqlImg from "@/public/images/icons/sql.png";
+import nosqlImg from "@/public/images/icons/nosql.png";
+import gitImg from "@/public/images/icons/git.png";
+import cliImg from "@/public/images/icons/cli.png";
+import dockerImg from "@/public/images/icons/docker.png";
+import postgresqlImg from "@/public/images/icons/postgresql.png";
+import mongodbImg from "@/public/images/icons/mongodb.png";
 
 const Experience = () => {
-  const languages = ["JavaScript", "Python"];
+  const languages = [
+    { name: "javaScript", img: jsImg },
+    { name: "typescript", img: tsImg },
+    { name: "Python", img: pythonImg },
+  ];
 
   const frontEnd = [
-    "TypeScript",
-    "React",
-    "NextJS",
-    "Redux",
-    "CSS",
-    "Tailwind CSS",
-    "Bootstrap",
-    "Git",
-    "CLI/Shell Scripting",
+    { name: "react", img: reactImg },
+    { name: "nextJS", img: nextjsImg },
+    { name: "redux", img: reduxImg },
+    { name: "HTML", img: htmlImg },
+    { name: "CSS", img: cssImg },
+    { name: "tailwind CSS", img: tailwindImg },
+    { name: "bootstrap", img: bootstrapImg },
+    { name: "material UI", img: materialImg },
   ];
 
   const backEnd = [
-    "SQL/NOSQL",
-    "MongoDB",
-    "NodeJS",
-    "ExpressJS",
-    "API",
-    "Docker",
-    "Software testing",
+    { name: "nodeJS", img: nodejsImg },
+    { name: "expressJS", img: expressjsImg },
+    { name: "API", img: apiImg },
+    { name: "SQL", img: sqlImg },
+    { name: "NOSQL", img: nosqlImg },
+  ];
+
+  const devops = [
+    { name: "git", img: gitImg },
+    { name: "CLI", img: cliImg },
+    { name: "docker", img: dockerImg },
+  ];
+
+  const database = [
+    { name: "PostgreSQL (SQL)", img: postgresqlImg },
+    { name: "MongoDB (NOSQL)", img: mongodbImg },
   ];
   return (
     <div className="w-full flex flex-col justify-center items-center px-[10%] md:px-[20%]">
       <h1 className="text-3xl font-bold m-5">Experience</h1>
-      <div className="w-full border-2 border-gBlue bg-gBlue bg-opacity-10 rounded-lg">
+      <div className="w-full border-2 border-gBlue bg-gBlue bg-opacity-10 rounded-lg p-4">
         <ExpField title={"programming langauage"} techs={languages} />
         <ExpField title={"front End"} techs={frontEnd} />
         <ExpField title={"Back End"} techs={backEnd} />
+        <ExpField title={"database"} techs={database} />
+        <ExpField title={"DevOps"} techs={devops} />
       </div>
     </div>
   );
